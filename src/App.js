0a5e8bcf1963c7e8995ef46a43439b80300c57e1
Parth Radia <parthnradia@gmail.com>
2018-08-06 15:06:58 -0700

    inital commit; installed modularized antd enterprise react framework

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Button type="primary">Button</Button>
+			</div>
+		);
+	}
+}
+
+export default App;
